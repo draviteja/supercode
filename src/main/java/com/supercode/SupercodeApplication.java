7cc9e67b3c2b414ad5529c6904298f8c9c06f52a
draviteja <raviteja5255@gmail.com>
2019-03-25 16:29:06 +0530

    spring boot app created

---
+++ src/main/java/com/supercode/SupercodeApplication.java
@@ -0,0 +1,13 @@
+package com.supercode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupercodeApplication.class, args);
+	}
+
+}
